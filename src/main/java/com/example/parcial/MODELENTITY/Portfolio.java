9b6a51d2141499c444a09c1db5727b3d4f02c57e
Pablogarciao <pgarciao2002@gmail.com>
2024-09-08 10:45:05 -0500

    Commit merging everything

--- src/main/java/com/example/parcial/MODELENTITY/Portfolio.java
+++ src/main/java/com/example/parcial/MODELENTITY/Portfolio.java
@@ -1,5 +1,6 @@
 package com.example.parcial.MODELENTITY;
 
+import com.example.parcial.UTILS.CompositeKey;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
@@ -13,23 +14,22 @@ import java.util.List;
 @Entity
 @Table(name="portafolio")
 public class Portfolio {
+    @EmbeddedId
+    private CompositeKey id;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)  // PK: ID
-    private Long id;
-
-
-    // FK hacia User
     @ManyToOne
-    @JoinColumn(name = "id_user", nullable = false)  // FK: IDUser en el diagrama
-    private Usuario user;
+    @JoinColumn(name = "id_user", insertable = false, updatable = false)
+    private User user;
 
     // Relación con PortfolioMedia (1 Portfolio puede tener varios PortfolioMedia)
     @OneToMany(mappedBy = "portfolio", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<PortfolioMedia> media;
 
-    @Column(name = "biography")
+    @Column()
     private String biography;
+
+    @ManyToMany ()@JoinTable(        name="participations",        joinColumns = @JoinColumn(name="id_portfolio"),
+            inverseJoinColumns = @JoinColumn(name="id_event"))private List<Event> participations;
 
 }
 
